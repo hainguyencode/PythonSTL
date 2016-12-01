0d9b043ab73ccf53448e0eaa2a80f860e0b584a4
hainguyencode <hainguyen.code@gmail.com>
2016-12-01 17:09:33 +0700

    test join()

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os.path
 import commands
+from matplotlib.font_manager import path
 
 
 def run(command):
@@ -16,14 +17,22 @@ def run(command):
 #test run function
 # print run("ps -au $(whoami)")
 
+path = "ls -la"
+cmd = 'pwd'
+
 # test split()
-# cmd = "pwd"
 # print os.path.split(run(cmd)) #return tuple: dirname + basename
 # print os.path.basename(run("pwd")) #file name
 # print os.path.dirname(run("pwd"))   #parent path
 
 #test splittext
 # print os.path.splitext("/usr/local/test.txt") # --> ('/usr/local/test', '.txt')
+
+#test join: to buid path
+for parts in [ ('/', 'usr', 'local') ]:
+    print os.path.join(*parts)
+cmd = path + ' ' + os.path.join(*parts) 
+print run(cmd)
 
 
 
